f31a9b2f50eb48b48284a01a46ea4505181928ca
MohamedElasyad <melasyad@snih.gov.sa>
2023-07-27 14:38:19 +0300

    conti in adjusting and implementing jquery data table functionalities

--- Core.Web/Controllers/HomeController.cs
+++ Core.Web/Controllers/HomeController.cs
@@ -1,22 +1,68 @@
-﻿using Core.Web.Models;
+﻿using Core.Web.Data;
+using Core.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.Linq.Dynamic.Core;
 
 namespace Core.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IApplicationDbContext context)
         {
             _logger = logger;
+            this._context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = _context.Customers.ToList();
+            return View(model);
         }
+
+
+        [HttpPost]
+        public JsonResult GetAllCustomers()
+        {
+            int pageLength = int.Parse(Request.Form["length"]);
+            int start = int.Parse(Request.Form["start"]);
+            int skip = start;
+            int pageNumber = int.Parse(Request.Form["draw"]);
+            string searchText = Request.Form["search[value]"];
+
+            string sortColumn = Request.Form["order[0][column]"];
+            //var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+
+            string sortDir = Request.Form["order[0][dir"];
+
+
+            var model = from customer in _context.Customers select customer;
+
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortDir)))
+            {
+                model = model.OrderBy(sortColumn + " " + sortDir);
+            }
+
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                model = model
+                    .Where(x => string.IsNullOrEmpty(searchText) ? true :
+                (x.FirstName.Contains(searchText) || x.LastName.Contains(searchText) || x.Contact.Contains(searchText) || x.Email.Contains(searchText)));
+            }
+
+            var data = model
+                .Skip(start).Take(pageLength).ToList();
+
+
+            int totalRecords = model.Count();
+            var jsonData = new { data = data, recordsFiltered = totalRecords };
+            return Json(jsonData);
+        }
+
 
         public IActionResult Privacy()
         {
--- Core.Web/wwwroot/js/customerDatatable.js
+++ Core.Web/wwwroot/js/customerDatatable.js
@@ -1,6 +1,40 @@
 $(document).ready(function () {
 
 
-    $('#customerDatatable').DataTable();
+    $('#customerDatatable').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "filter": true,
+        "ajax": {
+            "url": "/home/GetAllCustomers",
+            "type": "post",
+            "dataType": "json",
+
+        },
+        "columnDefs": [
+            {
+                "targets": [0],
+                "visible": true,
+                "searchable": false
+            }
+        ],
+        "columns": [
+            { "data": "id", "name": "Id", "autoWidth": true },
+            { "data": "firstName", "name": "First Name", "autoWidth": true },
+            { "data": "lastName", "name": "Last Name", "autoWidth": true },
+            { "data": "contact", "name": "Contact", "autoWidth": true },
+            { "data": "email", "name": "Email", "autoWidth": true },
+            { "data": "dateOfBirth", "name": "Date Of Birth", "autoWidth": true },
+            {
+                "render": function (data, row) {
+                    var btnDelete = `<a class='btn btn-danger btn-xs' onclick="alert(${row})""> Delete</a>`
+                    return btnDelete;
+                }
+            },
+        ]
+
+
+
+    });
 
 });
